5d04b41a560076c0494a0bd6d552ecca973f9a89
Giyanellow <montenegroblends@gmail.com>
2024-07-24 16:26:18 +0800

    Switched to BeatifulSoup instead of Selenium and was able to access information within the webpage
    To do:
    - Handle edge cases for information outside of span tags
    - Add information to a pandas df for easier conversion to csv
    - make a streamlined ETL pipeline for analysis

--- scrape.py
+++ scrape.py
@@ -1,24 +1,62 @@
+""" 
+What to find:
+1. Date
+2. Temeperature (High and Low)
+3. Condition
+4. Precipitation Probability
+5. Real Feel
+6. Real Feel Shade
+7. UV Index
+8. Wind Speed
+"""
+
+
 import time
 import requests
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+import csv
+from bs4 import BeautifulSoup
+import pandas as pd
+
 
 url = "https://www.accuweather.com/en/ph/baguio-city/1-262309_1_al/daily-weather-forecast/1-262309_1_al"
 
+
 def main():
-    if check_status(url):
-        ...
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 OPR/111.0.0.0'}
+    request = requests.get(url, headers=headers)
+    status = request.status_code
+    if status == 200:
+        scrape(request)
+
     
+def scrape(request):
+    soup = BeautifulSoup(request.content, "html.parser")
+    title = soup.find("title").text
+    print(title)
     
-def check_status(url):
-    if requests.get(url) == 200:
-        return True
-    return False
-
-def scrape(url):
-    driver = webdriver.Chrome()
-    driver.get(url)
-
-
-if "__name__" == "__main__":
+    card_class = "daily-wrapper"
+    to_scrape = {
+        "date": "date",
+        "high": "high",
+        "low": "low",
+        "precipitation": "precipitation",
+        "condition" : "phrase"
+    }
+    divs = soup.find_all("div", class_=card_class)
+    
+    for div in divs:
+        data = {}
+        for key, value in to_scrape.items():
+            try:
+                element = div.find("span", class_=value)
+                if element:
+                    data[key] = element.text
+                else:
+                    data[key] = div.find("div", class_=value).text
+            except:
+                data[key] = "N/A"
+        print(data)
+        print("\n")
+    
+if __name__ == "__main__":
     main()
