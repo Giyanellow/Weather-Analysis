87991400665612507c2c96f275864e8a19f84e2e
Giyanellow <montenegroblends@gmail.com>
2024-07-24 17:21:22 +0800

    Successful scraping of data from accuweather page, exported to initial csv (weather_data.csv)
    To do:
    - Clean scraped data

--- scrape.py
+++ scrape.py
@@ -16,17 +16,21 @@ import requests
 import csv
 from bs4 import BeautifulSoup
 import pandas as pd
+import re
 
 
 url = "https://www.accuweather.com/en/ph/baguio-city/1-262309_1_al/daily-weather-forecast/1-262309_1_al"
-
 
 def main():
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 OPR/111.0.0.0'}
     request = requests.get(url, headers=headers)
     status = request.status_code
     if status == 200:
-        scrape(request)
+        weather_df = scrape(request)
+    
+    weather_df.to_csv("weather_data.csv", index=False)
+        
+    
 
     
 def scrape(request):
@@ -36,27 +40,61 @@ def scrape(request):
     
     card_class = "daily-wrapper"
     to_scrape = {
-        "date": "date",
+        "date": "module-header sub date",
         "high": "high",
         "low": "low",
-        "precipitation": "precipitation",
+        "precipitation": "precip",
         "condition" : "phrase"
     }
+    
+    special_cases = {
+        "real feel": "RealFeel®",
+        "real feel shade": "RealFeel Shade™",
+        "max uv index": "Max UV Index",
+        "wind": "Wind"
+    }
     divs = soup.find_all("div", class_=card_class)
+    
+    weather_data = []
     
     for div in divs:
         data = {}
         for key, value in to_scrape.items():
             try:
+                if key == "precipitation":
+                    element = div.find("div", class_=value)
+                    if element:
+                        data[key] = element.text.replace('\n', '').replace('\t', '').strip()
+                    else:
+                        data[key] = "N/A"
+                    continue
+                
                 element = div.find("span", class_=value)
                 if element:
-                    data[key] = element.text
+                    data[key] = element.text.replace('\n', '').replace('\t', '').strip()
                 else:
                     data[key] = div.find("div", class_=value).text
             except:
                 data[key] = "N/A"
-        print(data)
-        print("\n")
+                print(f"Exception {key}: {value}")
+                
+        panel_items = div.find_all("p", class_="panel-item")        
+        for item in panel_items:
+            try:
+                label = item.contents[0].strip()
+                span = item.find("span", class_="value")
+                if span:
+                    for key, special_label in special_cases.items():
+                        if special_label in label:
+                            data[key] = span.text.strip()
+                            break
+            except Exception as e:
+                print(f"Exception for special case in panel-item: {e}")
+                
+        weather_data.append(data)
+        
+    weather_df = pd.DataFrame(weather_data)
+    return weather_df
     
 if __name__ == "__main__":
     main()
